8e8bb07d5d4591ae9d2a5f1d8f7f29700b0eab4d
Denise de Fátima Gonçalves <denise.goncalves.v@gmail.com>
2026-07-17 13:30:40 -0300

    Feat: Animação de Ataque com o Machado
    Foi adicionado no script a condição que aciona a animação de ataque com o machado (Axe) quando clica com o botão direito do mouse. Porém, quando se faz isso a movimentação do player não volta sozinha, sendo necessário acessar o animator do AXE e criar um script (ActionDone.cs) que chamao método ActionDone dentro do PlayerController.cs, deixando o isAction como falso, voltando a movimentação normal após a animação.

--- Like Forager/Assets/Scripts/PlayerController.cs
+++ Like Forager/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour
 
     private bool isLookLeft = true;
     private bool isWalk;
+    private bool isAction;
 
     public float movementSpeed;
 
@@ -30,15 +31,30 @@ public class PlayerController : MonoBehaviour
         {
             Flip(); //Se o mouse estiver à esquerda do personagem e ele não estiver olhando para a esquerda, chama a função Flip
         }
+
         if(mousePosition.x > transform.position.x && isLookLeft == true)
         {
             Flip(); //Se o mouse estiver à direita do personagem e ele estiver olhando para a esquerda, chama a função Flip
         }
 
+        if(Input.GetButtonDown("Fire1") && isAction == false) 
+        {
+            isAction = true;
+            m_Animator.SetTrigger("Axe");
+        }
+
         movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         isWalk = movementInput.magnitude != 0; //Se o movimento for diferente de zero, isWalk será verdadeiro, caso contrário, falso
 
-        m_Rigidbody.velocity = movementInput * movementSpeed; 
+        if(isAction == false)
+        {
+            m_Rigidbody.velocity = movementInput * movementSpeed; 
+        }
+        else if(isAction == true)
+        {
+            m_Rigidbody.velocity = Vector2.zero;
+            isWalk = false;
+        }
 
         m_Animator.SetBool("isWalk", isWalk);    
         
@@ -46,8 +62,22 @@ public class PlayerController : MonoBehaviour
 
     private void Flip()
     {
+        if(isAction == true)
+        {
+            return; //Se o personagem estiver realizando uma ação, não permite que ele vire
+        }
         isLookLeft = !isLookLeft; //Inverte o valor de isLookLeft
         float x = transform.localScale.x * -1; //Inverte a escala do personagem no eixo x
         transform.localScale = new Vector3(x, 1, 1); //Aplica a nova escala ao personagem
+    }
+
+    public void AxeHit()
+    {
+        
+    }
+
+    private void ActionDone()
+    {
+        isAction = false; //Quando a ação terminar, permite que o personagem se mova novamente
     }
 }
