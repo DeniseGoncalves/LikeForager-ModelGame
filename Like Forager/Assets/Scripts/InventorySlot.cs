95f21838d5e168a7b924a6199b38d5a6e1b74990
Denise de Fátima Gonçalves <denise.goncalves.v@gmail.com>
2026-09-15 16:52:12 -0300

    Feat: Prefab do Slot
    Foi criado um script denominado InventorySlot.cs no qual foi realizado as primeiras programações - deixando as funções de interação com o mouse preparadas para inserir as informações dos loots - e finalizou-se transformando o slot em prefab.

---
+++ Like Forager/Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    [SerializeField] private Item item;
+    public Image itemImage;
+    public Text amountTxt;
+   
+   public void UpdateSlot(Item i, int amount)
+    {
+        item = i;
+        itemImage.sprite = item.itemSprite;
+        amountTxt.text = amount.ToString();
+    }
+
+    public void OnSlotClick() // Função para verificar se o item é consumível ou não
+    {
+        
+    }
+
+    public void MouseEnter()
+    {
+        
+    }
+
+    public void MouseExit()
+    {
+        
+    }
+}
